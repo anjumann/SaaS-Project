6dc1b6bd03ad71d0fcf922575a3238c632131d01
Anjuman Raj <ayusmann23@gmail.com>
2023-06-10 00:12:41 +0530

    ✨ feat(package.json): add framer-motion dependency ✨ feat(pages): add new pages for career, portfolio, services, team, admin dashboard, admin login, and error ✨ feat(layout.js): update metadata for RootLayout ✨ feat(app): add new components for Home page (Hero, Services, Portfolio, Testimonials, About, FAQ) and Footer component
    ✨ feat(components): add new components to Home page
    - Add About, CTA, FAQ, Hero, Portfolio, QuickLink, Services, and Testimonials components to Home page
    - Export all components from Home index file
    ✨ feat(components/ui): add new UI components
    - Add Button, Card, and Heading components to UI components
    ✨ feat(components): add Navigation component
    - Add Navigation component to components directory

---
+++ src/components/Home/index.js
@@ -0,0 +1,10 @@
+import Hero from "@/components/Home/Hero";
+import Portfolio from "@/components/Home/Portfolio";
+import Services from "@/components/Home/Services";
+import Testimonials from "@/components/Home/Testimonials";
+import About from "@/components/Home/About";
+import FAQ from "@/components/Home/FAQ";
+import CTA from "@/components/Home/CTA"
+import QuickLink from "@/components/Home/QuickLink"
+
+export { Hero, Portfolio, Services, FAQ, Testimonials, About, QuickLink, CTA}
