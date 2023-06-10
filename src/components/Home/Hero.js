11fecb1e0b66de641fe5918b72a7e1a905b615ec
Anjuman Raj <ayusmann23@gmail.com>
2023-06-10 23:47:58 +0530

    ✨ feat(package.json): add react-intersection-observer dependency ✨ feat(framermotion.js): add TypingText component to create typing effect on text

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -1,11 +1,16 @@
 import Button from "@/components/ui/Button"
+import { TypingText } from "@/utils/framermotion"
 
 
 const Hero = () => {
   return (
     <>
       <div className="">
-        <Button/>
+
+      <TypingText>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit debitis error molestias corporis consequatur!
+      </TypingText>
+
       </div>
     </>
   )
