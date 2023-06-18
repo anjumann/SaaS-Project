92698d0f8805ddd6a63ee3d07db870131859306b
Anjuman Raj <ayusmann23@gmail.com>
2023-06-18 22:58:51 +0530

    ✨ feat(.env): add DATABASE_URL environment variable ✨ feat(package.json): add @prisma/client and prisma dev dependency ✨ feat(prisma/schema.prisma): add User, Project, Team, Link, and Services models ✨ feat(src/app/api/project/(api)/route.js): add GET method to fetch photos from jsonplaceholder API

---
+++ src/app/api/project/(api)/route.js
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server'
+
+export async function GET(request) {
+    const { searchParams } = new URL(request.url)
+    const res = await fetch('https://jsonplaceholder.typicode.com/photos', {
+        next: {
+            revalidate: 60,
+        }
+    })
+    const page = searchParams.get('page') || 1
+    const data = await res.json()
+    const pageSize = 10
+    const totalPage = Math.ceil(data.length / pageSize)
+    const photos = data.slice((pageSize * (page-1) ), pageSize * page)
+    
+    return NextResponse.json({ "data": photos, "size": photos.length, "totalPage": totalPage })
+}
