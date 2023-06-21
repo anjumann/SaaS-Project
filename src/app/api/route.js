62d00e9b63ad1826663badbf131cb8a40574d79c
Anjuman Raj <ayusmann23@gmail.com>
2023-06-22 00:22:25 +0530

    🐛 fix(route.js): fix the repoList array mapping ✨ feat(route.js): add more Github info to the response

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -3,13 +3,25 @@ import { NextResponse } from 'next/server'
 export async function GET(request) {
 
     const data = await fetch('https://api.github.com/users/anjumann').then(res => res.json())
+    const repo = await fetch('https://api.github.com/users/anjumann/repos').then(res => res.json())
 
-
+    const repoList = [repo.map((item) => {
+        return {
+            'name': item.name,
+            'url': item.html_url
+        }
+    })]
 
     return NextResponse.json({
         Message: 'Welcome to the APIs',
         Creater: 'Anjuman Raj',
-        "Github info": JSON.stringify(data)
+        "Github info": {
+            "Name": data.name,
+            "userName": data.login,
+            "Bio": data.bio,
+            "Github URl": data.html_url,
+            "Repo": repoList
+        }
     })
 }
 
